36fe7b0fa20915ce9a1878c68ad5921a5021febd
cameron <s1458162@sms.ed.ac.uk>
2018-11-16 10:18:39 +0000

    cloud ready

--- src/configs/schedules/round_10/r001.py
+++ src/configs/schedules/round_10/r001.py
@@ -9,8 +9,4 @@ from src.configs.resnet.dim256x1 import g_structure
 from src.configs.patchgan.dim256x2_70_nobn_nosig import d_structure
 
 schedule = Schedule(name)
-schedule['batch_size'] = 1
 schedule['debug_plot'] = True
-schedule['save_img_interval'] = 1
-schedule['n_test'] = 1
-schedule['save_summary']['n'] = 4
